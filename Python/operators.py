e17251736d7dc0e25ced883e72d37a4e8a454c17
nannau <nannau@uvic.ca>
2019-03-29 11:03:32 -0700

    Updated documentation:

--- Python/operators.py
+++ Python/operators.py
@@ -36,9 +36,11 @@ def center_data(data_cube):
     with warnings.catch_warnings():
         warnings.simplefilter("ignore", category=RuntimeWarning)
         mean = data_cube['dv'].mean(dim='run', skipna=True)
-        data_cube['dv'] = data_cube['dv'] - mean
 
-    return data_cube
+        X = data_cube
+        X['dv'] = data_cube['dv'] - mean
+
+    return X
 
 def weight_by_area(data_cube):
     """Weights each CanRCM4 grid cell 
@@ -47,12 +49,16 @@ def weight_by_area(data_cube):
     Args: 
         data_cube (xarray Dataset): datacube 
             containing an ensemble of 
-            CanRCM4 models  
+            CanRCM4 models 
+    Returns:
+        X (xarray Dataset): design value datacube 
+            weighted by area
     """
+ 
 
     check_keys(data_cube)
     n, p = cell_count(data_cube)
-    
+
     # calculate mean size of grid cell
     rlat = np.diff(data_cube['rlat']).mean()
     rlon = np.diff(data_cube['rlon']).mean()
@@ -63,10 +69,13 @@ def weight_by_area(data_cube):
     lon_sz = np.abs(np.deg2rad(rlon))
 
     # calculate rectangular area on sphere.
+    # note that radius wolud cancel out 
+    # in fraction
     area = (lat_sz * lon_sz)
     total_area = area*p
     frac_area = area/total_area
 
-    data_cube['dv'] = data_cube['dv']*frac_area
+    X = data_cube
+    X['dv'] = data_cube['dv']*frac_area
     
-    return data_cube
+    return X
