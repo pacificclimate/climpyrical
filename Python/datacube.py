7be81d549db093eee352d2a446268f2872e92f80
nannau <nannau@uvic.ca>
2019-03-29 10:55:42 -0700

    Separated out files. New EOF method in testing.ipynb

---
+++ Python/datacube.py
@@ -0,0 +1,76 @@
+import glob
+
+import numpy as np
+import xarray as xr
+import netCDF4 as nc
+
+def read_data(data_path, dv_key_name):
+    """Load an ensemble of CanRCM4
+    models into a single datacube.
+    ------------------------------
+    Args:
+        data_path (Str): path containing CanRCM4 ensemble
+        dv_key_name (Str): name of the design value as 
+            in the netCDF file
+    Returns:
+        ds (xarray Dataset): data cube of assembled ensemble models 
+            into a single variable
+    """
+    if not isinstance(data_path, str):
+        raise TypeError("data_path must be a string, got {}"
+                        .format(type(data_path)))
+
+    if not isinstance(dv_key_name, str):
+        raise TypeError("dv_key_name must be a string, got {}"
+                        .format(type(dv_key_name)))
+
+    # Create a list of all files in PATH
+    nc_list = np.asarray(glob.glob(data_path+"*.nc"))
+
+    for path in nc_list:
+        if path.endswith('.nc') is False:
+            raise IOError('{} is not a supported file type. Data directory \
+                          must have only .nc files'.format(path))
+
+    # create an example NetCDF4 dataset
+    inst = nc.Dataset(nc_list[0], 'r')
+    data_cube = np.ones((inst['lat'].shape[0],
+                         inst['lat'].shape[1],
+                         nc_list.shape[0]))
+
+    inst.close()
+
+    # iterate through files in path
+    # to create data cube
+    for i, path in enumerate(nc_list):
+        run = nc.Dataset(path, 'r')
+        dv = run.variables[dv_key_name][:, :]
+        data_cube[:, :, i] = dv
+
+    lat = run.variables['lat'][:, :]
+    lon = run.variables['lon'][:, :]
+
+    rlat = run.variables['rlat'][:]
+    rlon = run.variables['rlon'][:]
+
+    run.close()
+
+    # constrube datacube as an xarray dataset
+    ds = xr.Dataset({'dv': (['x', 'y', 'run'], data_cube)},
+                    coords={'lon': (['x', 'y'], lon),
+                            'lat': (['x', 'y'], lat),
+                            'rlon': rlon,
+                            'rlat': rlat},
+                    attrs={'dv': 'mm h-1',
+                           'lon': 'degrees',
+                           'lat': 'degrees',
+                           'rlon': 'degrees',
+                           'rlat': 'degrees'})
+
+    return ds
+
+class DataReader:
+    def __init__(self, data_path, dv_key_name):
+        self.data_path = data_path
+        self.dv_key_name = dv_key_name
+        self.read_data = read_data(data_path, dv_key_name)
---
+++ Python/operators.py
@@ -0,0 +1,72 @@
+import warnings
+
+import xarray as xr
+import numpy as np
+
+def check_keys(data_cube):
+    rlat = 'rlat' in data_cube.variables
+    rlon = 'rlon' in data_cube.variables
+    run = 'run' in data_cube.variables
+    if not rlat and rlon and run:
+        raise KeyError("CanRCM4 ensemble is missing 'run' axis, 'rlon' \
+                        and 'rlat'. Please ensure the model is properly \
+                        gridded with these names.")
+
+def cell_count(data_cube):
+    check_keys(data_cube)
+    n = data_cube['run'].shape[0]
+    p = data_cube['rlat'].shape[0]*data_cube['rlon'].shape[0]
+    return n, p
+
+def center_data(data_cube):
+    """Centers each run in data_cube
+    by subtracting ensemble mean at
+    a given grid cell.
+    --------------------------------
+    Args: 
+        data_cube (xarray Dataset): datacube 
+            containing an ensemble of 
+            CanRCM4 models
+    Returns:
+        X (xarray Dataset): mean centered
+            datacube  
+    """
+    check_keys(data_cube)
+
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore", category=RuntimeWarning)
+        mean = data_cube['dv'].mean(dim='run', skipna=True)
+        data_cube['dv'] = data_cube['dv'] - mean
+
+    return data_cube
+
+def weight_by_area(data_cube):
+    """Weights each CanRCM4 grid cell 
+    by fractional area.
+    --------------------------------
+    Args: 
+        data_cube (xarray Dataset): datacube 
+            containing an ensemble of 
+            CanRCM4 models  
+    """
+
+    check_keys(data_cube)
+    n, p = cell_count(data_cube)
+    
+    # calculate mean size of grid cell
+    rlat = np.diff(data_cube['rlat']).mean()
+    rlon = np.diff(data_cube['rlon']).mean()
+
+    # calulate rectangular size of
+    # grid cell 
+    lat_sz = np.abs(np.sin(np.deg2rad(rlat)))
+    lon_sz = np.abs(np.deg2rad(rlon))
+
+    # calculate rectangular area on sphere.
+    area = (lat_sz * lon_sz)
+    total_area = area*p
+    frac_area = area/total_area
+
+    data_cube['dv'] = data_cube['dv']*frac_area
+    
+    return data_cube
