1a7da8964596cf8fb52892f11d393eeb9fe4a2d6
nannau <nannau@uvic.ca>
2019-04-01 13:54:53 -0700

    Adds documenation to eof steps

--- Python/eof.py
+++ Python/eof.py
@@ -5,67 +5,187 @@ from sklearn import linear_model
 from operators import *
 
 def ensemble_reshape(data_cube):
+    """Reshapes data cube into array shape 
+    for eof calculations. 
+    --------------------------------
+    Args: 
+        data_cube (xarray Dataset): datacube 
+            containing an ensemble of 
+            CanRCM4 models 
+    Returns:
+        field_ens (numpy.ndarray): reshaped 2-d array
+            into number of cells x ensemble size
+    """
     check_keys(data_cube)
+
     x_field = data_cube['dv']
     n, p = cell_count(data_cube)
     field_ens = np.reshape(x_field.values, (p, n))
+
     return field_ens
 
 def get_land(ensemble_arr):
+    """Gets indeces of nan values to 
+    identify where land or ocean is. 
+    --------------------------------
+    Args: 
+        ensemble_arr (numpy.ndarray): reshaped 2-d array
+            into number of cells x ensemble size
+    Returns:
+        (numpy.ndarray): 2-d array containing
+            the indeces of nan values
+    """
     return np.isnan(ensemble_arr)
 
 def rand_sample_index(p, frac):
+    """Randomly sample a range of integers. 
+    --------------------------------
+    Args: 
+        p (int): maximum integer to sample
+        frac (float): fractional size of p 
+            to sample
+    Returns:
+        (numpy.ndarray): array containing indeces from 
+            a fraction of 0 to p
+    """
     index = np.random.choice(np.arange(p), int(frac*p))
     return index
 
 def pseudo_obs(data_cube, frac=0.02):
-    
+    """Randomly sample a data cube to generate
+    pseudo observations from ensemble. 
+    --------------------------------
+    Args:  
+        data_cube (xarray Dataset): datacube 
+            containing an ensemble of 
+            CanRCM4 models 
+    Returns:
+        y_sample (numpy.ndarray): fraction
+            of random spatially sampled design
+            value grid cells
+        index (numpy.ndarray): index locations 
+            of y_sample from original ensemble size
+    """
     Y_ens = ensemble_reshape(data_cube)
     land_idx = get_land(Y_ens)
+    # select only land
     Y_land = Y_ens[~land_idx.any(axis=1)]
 
     p, n = Y_land.shape
+
+    # randomly select an ensemble member
+    # to spatially sample
     i = np.random.randint(0, n-1)
     y_land = Y_land[:, i]
 
+    # select random indeces
     index = rand_sample_index(p, frac)
     y_sample = y_land[index]
 
     return y_sample, index
 
 def ensemble_to_eof(ensemble_arr):
+    """Perform EOF/PCA dimensionality reduction
+    on ensemble array 
+    --------------------------------
+    Args:  
+        ensemble_arr (numpy.ndarray): reshaped 2-d array
+            into number of cells x ensemble size
+    Returns:
+        eofs (numpy.ndarray): EOF transformed
+            ensemble_arr sorted by max variance 
+            explained
+    """
+    
+    # select only land from ensemble
     land_idx = get_land(ensemble_arr)
     ensemble_arr = ensemble_arr[~land_idx.any(axis=1)]
-    skpca = pca.PCA()
-    # transform to get proper shape for EOF dim reduction
     
+    skpca = pca.PCA()
+    
+    # transform to get proper shape for EOF dim reduction
     skpca.fit_transform(ensemble_arr.T)
+    
     # transform back to familiar shape n x p
     eofs = skpca.components_.T
+    
     return eofs
 
 def regress_eof(eofs, obs):
+    """Perform a linear regression between
+    EOFs and observations 
+    --------------------------------
+    Args:  
+        eofs (numpy.ndarray): EOF transformed
+            ensemble_arr containing same grid cells 
+            sample as observations 
+        obs (numpy.ndarray): Gridded observations
+            either from pseudo or real observations
+    Returns:
+        model (scikit-learn model obj): fitted 
+            model object to eofs and obs
+    """
     lm = linear_model.LinearRegression()
     model = lm.fit(eofs, obs)
-    print(model.score(eofs, obs))
+    print("Model score:", model.score(eofs, obs))
     return model
 
 def predict_dv(model, eofs_of_model):
+    """Reconstruct design value field from
+    full EOF sample 
+    --------------------------------
+    Args:  
+        model (scikit-learn model obj): fitted 
+            model object to eofs and obs
+        eofs_of_model (numpy.ndarray): EOF transformed
+            ensemble_arr containing all grid
+            cells in design value field
+    Returns:
+        (numpy.ndarray): predicted design
+            value field
+    """
     return model.predict(eofs_of_model)
 
 def reconstruct_eof_full(predictions, data_cube):
-
+    """Add EOF reconstructed predictions
+    to all grid cells of CanRCM4 model and 
+    add to data cube 
+    --------------------------------
+    Args:  
+        predictions (numpy.ndarray): predicted design
+            value field
+        data_cube (xarray Dataset): datacube 
+            containing an ensemble of CanRCM4 models
+    Returns:
+        data_cube (xarray Dataset): datacube 
+            containing an ensemble of CanRCM4 models
+            with added eofs variable
+    """
     full_arr = ensemble_reshape(data_cube)
     land_idx = get_land(full_arr)
+    # replace land values with predictions
     full_arr[~land_idx.any(axis=1)][:, 0] = predictions
-    
+    # add predictions to data cube
     data_cube['eofs'] = full_arr[:, 0]
 
     return data_cube
 
 def fit_transform(data_cube):
-
+    """Perform all steps to reconstruct a 
+    design value field from pseudo observations
+    ---------------
+    Args: 
+        data_cube (xarray Dataset): datacube 
+            containing an ensemble of CanRCM4 models 
+    Returns:
+        data_cube (xarray Dataset): datacube 
+            containing an ensemble of CanRCM4 models
+            with added eofs variable
+    """
     y, index = pseudo_obs(data_cube)
+
+    data_cube = center_data(data_cube)
+    data_cube = weight_by_area(data_cube)
 
     ensemble = ensemble_reshape(data_cube)
     ensemble_eof = ensemble_to_eof(ensemble)
