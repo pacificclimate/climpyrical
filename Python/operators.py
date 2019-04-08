12f16f2af1e2b0b6b0cc6de918c73e048c295e92
nannau <nannau@uvic.ca>
2019-04-08 10:53:56 -0700

    Changes input  requirement to xarray.DataArray

--- Python/operators.py
+++ Python/operators.py
@@ -2,55 +2,42 @@ import warnings
 
 import numpy as np
 
-
-def check_keys(data_cube):
-    rlat = 'rlat' in data_cube.keys()
-    rlon = 'rlon' in data_cube.keys()
-    run = 'run' in data_cube.keys()
-    dv = 'dv' in data_cube.keys()
-    if not rlat and rlon and run and dv:
-        raise KeyError(("CanRCM4 ensemble is missing 'run', 'rlon',\
-                        'rlat', or 'dv'. Please ensure the model\
-                         is properly gridded with these names."))
-
-
-def ens_mean(data_cube):
+def ens_mean(dv_field):
     """Centers each run in data_cube
     by subtracting ensemble mean at
     a given grid cell.
     --------------------------------
     Args:
-        data_cube (xarray Dataset): datacube
-            containing an ensemble of
-            CanRCM4 models
+        dv_field (xarray.DataArray): datacube
+            containing design values of from
+            an ensemble of CanRCM4 models
     Returns:
         data_cube (xarray Dataset): mean centered
             datacube
     """
-    check_keys(data_cube)
 
     with warnings.catch_warnings():
         warnings.simplefilter("ignore", category=RuntimeWarning)
-        mean = data_cube['dv'].mean(dim='run', skipna=True)
+        mean = dv_field.mean(dim='run', skipna=True)
 
     return mean
 
 
-def frac_grid_area(data_cube, R=6371.0):
+def frac_grid_area(dv_field, R=6371.0):
     """Calculates the fractional area of
     each CanRCM4 cell.
     --------------------------------
     Args:
-        data_cube (xarray Dataset): datacube
-            containing an ensemble of
-            CanRCM4 models
+        dv_field (xarray.DataArray): datacube
+            containing design values of from
+            an ensemble of CanRCM4 models
+        R (float): radius of Earth in km
     Returns:
         A (numpy.ndarray): 2-dimensional array
             containing areas of each grid cell
     """
-    check_keys(data_cube)
-    rlat_o = data_cube['rlat'].values
-    rlon_o = data_cube['rlon'].values
+    rlat_o = dv_field['rlat'].values
+    rlon_o = dv_field['rlon'].values
 
     # approximate grid cell size
     gridsz_rlat = rlat_o[-1]+np.diff(rlat_o).mean()
