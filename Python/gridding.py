0d19bd8e54599df777b85959fbf81545610a442b
nannau <nannau@uvic.ca>
2019-06-19 16:45:49 -0700

    Adds euclidean distance function relative to ensemble index

---
+++ Python/gridding.py
@@ -0,0 +1,13 @@
+import numpy as np
+from scipy.spatial import distance
+
+
+def euclidean_dist_index(lat_lon_obs, lat_lon_ens):
+    dist_list = []
+    for i, coord in enumerate(lat_lon_obs):
+        dist_list.append(
+                        distance.cdist(lat_lon_ens,
+                                       [coord],
+                                       'euclidean').argmin()
+        )
+    return np.asarray(dist_list)
