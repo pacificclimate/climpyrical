5b09dad011f0955e9e16da329a764a26c686b0e5
nannau <nannau@uvic.ca>
2019-07-25 13:58:56 -0700

    Adds data_path cheker

--- scripts/datacube.py
+++ scripts/datacube.py
@@ -1,4 +1,5 @@
 import glob
+import warnings
 
 import numpy as np
 import numpy.ma as ma
@@ -11,6 +12,14 @@ def check_keys(ds):
     if required_keys > actual_keys:
         raise KeyError("CanRCM4 ensembles is missing keys {}".format(required_keys - actual_keys))
 
+def check_path(data_path):
+    if isinstance(data_path, str) is False:
+        raise ValueError('Path requires str got {}'.format(type(data_path)))
+    if len(glob.glob(data_path)) == 0:
+        raise IndexError('Path provided has no files with \'.nc\' extension')
+    if len(glob.glob(data_path)) <=20:
+        warnings.warn("Path has a low ensemble size with < 20 members")
+
 def read_data(data_path):
     """Load an ensemble of CanRCM4
     models into a single datacube.
@@ -22,7 +31,7 @@ def read_data(data_path):
         ds (xarray Dataset): data cube of assembled ensemble models
             into a single variable.
     """
-
+    check_path(data_path)
     nc_list = np.asarray(glob.glob(data_path))
     xr_list = [xr.open_dataset(path) for path in nc_list]
 
@@ -30,13 +39,13 @@ def read_data(data_path):
 
     # find design value key so that it can
     # be renamed to dv for references throughout
-    # map-xtreme project
+    # climpyrical project
     for var in list(ds.variables):
 
         grids = ['rlon', 'rlat', 'lon', 'lat', 'time_bnds', 'time', 'rotated_pole']
 
         if var not in grids and var in ds.variables:
-            print(var)
+
             ds = ds.rename({var: 'dv'})
             ds = ds.squeeze('time')
 
