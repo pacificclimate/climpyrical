c026f7b1298473f8aa1bc53be60327b42f330f17
nannau <nannau@uvic.ca>
2019-07-26 12:48:49 -0700

    Improves key checking

--- scripts/datacube.py
+++ scripts/datacube.py
@@ -6,23 +6,22 @@ import numpy.ma as ma
 
 import xarray as xr
 
-def check_in_keys(ds):
-    required_keys = {'rlat', 'rlon', 'lat', 'lon', 'run'}
-    actual_keys = set(ds.keys())
-    if required_keys > actual_keys:
-        raise KeyError("CanRCM4 ensemble is missing keys {}".format(required_keys - actual_keys))
+def check_keys(ds, required_keys):
+    actual_keys = set(ds.variables).union(set(ds.dims))
 
-def check_out_keys(ds):
-    required_keys = {'rlat', 'rlon', 'run', 'dv'}
-    actual_keys = set(ds.keys())
-    if required_keys > actual_keys:
-        raise KeyError("Climpyrical produced a dataset missing keys {}".format(required_keys - actual_keys))
+    if not set(required_keys).issubset(actual_keys):
+        raise KeyError(
+                    "CanRCM4 ensemble is missing keys {}"
+                    .format(required_keys - actual_keys)
+            )
 
 def check_path(data_path):
     if isinstance(data_path, str) is False:
         raise ValueError('Path requires str got {}'.format(type(data_path)))
     if len(glob.glob(data_path)) == 0:
-        raise IndexError('Path provided has no files with \'.nc\' extension')
+        raise ValueError('Path provided has no files with \'.nc\' extension')
+    if len(glob.glob(data_path)) == 1:
+        raise ValueError('At least 2 ensemble members required to EOF analysis')
     if len(glob.glob(data_path)) <=20:
         warnings.warn("Path has a low ensemble size with < 20 members")
 
@@ -42,11 +41,14 @@ def read_data(data_path):
             into a single variable.
     """
     check_path(data_path)
-    nc_list = np.asarray(glob.glob(data_path))
-    xr_list = [xr.open_dataset(path) for path in nc_list]
 
+    nc_list = np.asarray(glob.glob(data_path))
+    test_file = xr.open_dataset(nc_list[0])
+    check_keys(test_file, {'rlat', 'rlon', 'lat', 'lon'})
+
+    xr_list = [xr.open_dataset(path) for path in nc_list]
     ds = xr.concat(xr_list, 'run')
-    check_in_keys(ds)
+    check_keys(ds, {'rlat', 'rlon', 'lat', 'lon', 'run'})
 
     # find design value key so that it can
     # be renamed to dv for references throughout
@@ -62,8 +64,4 @@ def read_data(data_path):
                 check_time(ds)
                 ds = ds.squeeze('time')
 
-        elif var not in ds.variables:
-            raise KeyError("Invalid CanRCM4 model. Design value key not found.")
-
-    check_out_keys(ds)
     return ds
