fd869e38d4a92abb9255a7096b110a0a7cab35fd
nannau <nannau@uvic.ca>
2019-07-26 11:12:00 -0700

    Checks time dim, and in and out keys

--- scripts/datacube.py
+++ scripts/datacube.py
@@ -6,11 +6,17 @@ import numpy.ma as ma
 
 import xarray as xr
 
-def check_keys(ds):
+def check_in_keys(ds):
+    required_keys = {'rlat', 'rlon', 'lat', 'lon', 'run'}
+    actual_keys = set(ds.keys())
+    if required_keys > actual_keys:
+        raise KeyError("CanRCM4 ensemble is missing keys {}".format(required_keys - actual_keys))
+
+def check_out_keys(ds):
     required_keys = {'rlat', 'rlon', 'run', 'dv'}
     actual_keys = set(ds.keys())
     if required_keys > actual_keys:
-        raise KeyError("CanRCM4 ensembles is missing keys {}".format(required_keys - actual_keys))
+        raise KeyError("Climpyrical produced a dataset missing keys {}".format(required_keys - actual_keys))
 
 def check_path(data_path):
     if isinstance(data_path, str) is False:
@@ -19,6 +25,10 @@ def check_path(data_path):
         raise IndexError('Path provided has no files with \'.nc\' extension')
     if len(glob.glob(data_path)) <=20:
         warnings.warn("Path has a low ensemble size with < 20 members")
+
+def check_time(ds):
+    if len(ds['time']) != 1:
+        raise ValueError('Climpyrical can not take inputs as time series. Must be uni-dimensional in the time axis.')
 
 def read_data(data_path):
     """Load an ensemble of CanRCM4
@@ -36,6 +46,7 @@ def read_data(data_path):
     xr_list = [xr.open_dataset(path) for path in nc_list]
 
     ds = xr.concat(xr_list, 'run')
+    check_in_keys(ds)
 
     # find design value key so that it can
     # be renamed to dv for references throughout
@@ -47,10 +58,12 @@ def read_data(data_path):
         if var not in grids and var in ds.variables:
 
             ds = ds.rename({var: 'dv'})
-            ds = ds.squeeze('time')
+            if 'time' in ds.keys():
+                check_time(ds)
+                ds = ds.squeeze('time')
 
         elif var not in ds.variables:
             raise KeyError("Invalid CanRCM4 model. Design value key not found.")
 
-    check_keys(ds)
+    check_out_keys(ds)
     return ds
