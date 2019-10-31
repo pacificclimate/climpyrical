9d00bb6fb2e33debde1c49f9cbf3dc64e729324d
nannau <nannau@uvic.ca>
2019-10-31 15:05:26 -0700

    Renames data tests directories. Test shape does not check failures.

--- tests/test_datacube.py
+++ tests/test_datacube.py
@@ -5,7 +5,7 @@ sys.path.append('../modules/')
 from datacube import read_data, check_keys
 
 
-@pytest.mark.parametrize('actual_keys, required_keys,passed', [
+@pytest.mark.parametrize('actual_keys,required_keys,passed', [
     ({'rlat', 'rlon', 'dv', 4, 'lat', 'lon'}, {'rlat', 'rlon', 'dv', 4}, True),
     ({'rlat', 'rlon', True, 99999}, {'rlat', 'rlon', True, 99999}, True)])
 def test_check_keys(actual_keys, required_keys, passed):
@@ -18,17 +18,13 @@ def test_bad_keys(actual_keys, required_keys, passed):
     with pytest.raises(KeyError):
         checker = check_keys(actual_keys, required_keys)
 
-@pytest.mark.parametrize('data_path, design_value_name, keys, shape', [
-    ('./data/good/snw.nc', 'snw', {'rlat', 'rlon', 'lat', 'lon'}, (66, 130, 155)),
-    ('./data/good/hdd.nc', 'heating_degree_days_per_time_period', {'rlat', 'rlon', 'lat', 'lon', 'level'}, (35, 130, 155))])
+@pytest.mark.parametrize('data_path,design_value_name,keys,shape', [
+    ('./data/snw.nc', 'snw', {'rlat', 'rlon', 'lat', 'lon'}, (66, 130, 155)),
+    ('./data/hdd.nc', 'heating_degree_days_per_time_period', {'rlat', 'rlon', 'lat', 'lon', 'level'}, (35, 130, 155)),
+    ('./data/example1.nc', 'hyai', {'lon', 'lat'}, (27,)),
+    ('./data/example2.nc', 'tas', {'lat', 'lon'}, (1, 130, 155)),
+    ('./data/example3.nc', 'tas', {'lat', 'lon'}, (1, 128, 256)),
+    ('./data/example4.nc', 'tos', {'lat', 'lon'}, (24, 170, 180))])
 def test_shape(data_path, design_value_name, keys, shape):
     ds = read_data(data_path, design_value_name, keys)
     assert(ds[design_value_name].shape == shape)
-
-@pytest.mark.parametrize('data_path, design_value_name, keys, shape', [
-    ('./data/bad/example1.nc', 'hyai', {'lon', 'lat'}, (27,)),
-    ('./data/bad/example2.nc', 'tas', {'lat', 'lon'}, (1, 130, 155)),
-    ('./data/bad/example3.nc', 'tas', {'lat', 'lon'}, (1, 128, 256)),
-    ('./data/bad/example4.nc', 'tos', {'lat', 'lon'}, (24, 170, 180))])
-def test_bad_shape(data_path, design_value_name, keys, shape):
-    ds = read_data(data_path, design_value_name, keys)
