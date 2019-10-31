abc875f62fdbeff1f21a4743116c9d0929fe3166
nannau <nannau@uvic.ca>
2019-10-31 14:46:41 -0700

    Removes expected fail for shape test

--- tests/test_datacube.py
+++ tests/test_datacube.py
@@ -20,12 +20,15 @@ def test_bad_keys(actual_keys, required_keys, passed):
 
 @pytest.mark.parametrize('data_path, design_value_name, keys, shape', [
     ('./data/good/snw.nc', 'snw', {'rlat', 'rlon', 'lat', 'lon'}, (66, 130, 155)),
-    ('./data/good/hdd.nc', 'heating_degree_days_per_time_period', {'rlat', 'rlon', 'lat', 'lon', 'level'}, (35, 130, 155)),
+    ('./data/good/hdd.nc', 'heating_degree_days_per_time_period', {'rlat', 'rlon', 'lat', 'lon', 'level'}, (35, 130, 155))])
+def test_shape(data_path, design_value_name, keys, shape):
+    ds = read_data(data_path, design_value_name, keys)
+    assert(ds[design_value_name].shape == shape)
+
+@pytest.mark.parametrize('data_path, design_value_name, keys, shape', [
     ('./data/bad/example1.nc', 'hyai', {'lon', 'lat'}, (27,)),
     ('./data/bad/example2.nc', 'tas', {'lat', 'lon'}, (1, 130, 155)),
     ('./data/bad/example3.nc', 'tas', {'lat', 'lon'}, (1, 128, 256)),
     ('./data/bad/example4.nc', 'tos', {'lat', 'lon'}, (24, 170, 180))])
-@pytest.mark.xfail(raises=ValueError)
-def test_shape(data_path, design_value_name, keys, shape):
+def test_bad_shape(data_path, design_value_name, keys, shape):
     ds = read_data(data_path, design_value_name, keys)
-    assert(ds[design_value_name].shape == shape)
