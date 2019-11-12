24b88241e2b1c5876ee400ba642adae6b92f03f5
nannau <nannau@uvic.ca>
2019-11-12 13:32:36 -0800

    Flake 8

--- tests/test_datacube.py
+++ tests/test_datacube.py
@@ -1,15 +1,32 @@
-import sys, os
+import os
 import pytest
+from climpyrical.datacube import read_data
+from climpyrical.datacube import check_keys
 
-from climpyrical.datacube import read_data, check_keys
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
-@pytest.mark.parametrize('actual_keys,required_keys,passed', [
-    ({'rlat', 'rlon', 'dv', 4, 'lat', 'lon'}, {'rlat', 'rlon', 'dv', 4}, True),
-    ({'rlat', 'rlon', True, 99999}, {'rlat', 'rlon', True, 99999}, True),
-    ({'rlat', 'rlon', 4.0, False}, {'hi', 'nic', 'was', 'here', 'lon'}, False)])
+@pytest.mark.parametrize(
+    'actual_keys,required_keys,passed',
+    [
+        (
+            {'rlat', 'rlon', 'dv', 4, 'lat', 'lon'},
+            {'rlat', 'rlon', 'dv', 4},
+            True
+        ),
+        (
+            {'rlat', 'rlon', True, 99999},
+            {'rlat', 'rlon', True, 99999},
+            True
+        ),
+        (
+            {'rlat', 'rlon', 4.0, False},
+            {'hi', 'nic', 'was', 'here', 'lon'},
+            False
+        )
+    ]
+)
 def test_check_keys(actual_keys, required_keys, passed):
     if passed:
         assert check_keys(actual_keys, required_keys)
@@ -17,13 +34,48 @@ def test_check_keys(actual_keys, required_keys, passed):
         with pytest.raises(KeyError):
             check_keys(actual_keys, required_keys)
 
-@pytest.mark.parametrize('data_path,design_value_name,keys,shape', [
-    (os.path.join(THIS_DIR, './data/snw.nc'), 'snw', {'rlat', 'rlon', 'lat', 'lon'}, (66, 130, 155)),
-    (os.path.join(THIS_DIR, './data/hdd.nc'), 'heating_degree_days_per_time_period', {'rlat', 'rlon', 'lat', 'lon', 'level'}, (35, 130, 155)),
-    (os.path.join(THIS_DIR, './data/example1.nc'), 'hyai', {'lon', 'lat'}, (27,)),
-    (os.path.join(THIS_DIR, './data/example2.nc'), 'tas', {'lat', 'lon'}, (1, 130, 155)),
-    (os.path.join(THIS_DIR, './data/example3.nc'), 'tas', {'lat', 'lon'}, (1, 128, 256)),
-    (os.path.join(THIS_DIR, './data/example4.nc'), 'tos', {'lat', 'lon'}, (24, 170, 180))])
+
+@pytest.mark.parametrize(
+    'data_path,design_value_name,keys,shape',
+    [
+        (
+            os.path.join(THIS_DIR, './data/snw.nc'),
+            'snw',
+            {'rlat', 'rlon', 'lat', 'lon'},
+            (66, 130, 155)
+        ),
+        (
+            os.path.join(THIS_DIR, './data/hdd.nc'),
+            'heating_degree_days_per_time_period',
+            {'rlat', 'rlon', 'lat', 'lon', 'level'},
+            (35, 130, 155)
+        ),
+        (
+            os.path.join(THIS_DIR, './data/example1.nc'),
+            'hyai',
+            {'lon', 'lat'},
+            (27,)
+        ),
+        (
+            os.path.join(THIS_DIR, './data/example2.nc'),
+            'tas',
+            {'lat', 'lon'},
+            (1, 130, 155)
+        ),
+        (
+            os.path.join(THIS_DIR, './data/example3.nc'),
+            'tas',
+            {'lat', 'lon'},
+            (1, 128, 256)
+        ),
+        (
+            os.path.join(THIS_DIR, './data/example4.nc'),
+            'tos',
+            {'lat', 'lon'},
+            (24, 170, 180)
+        )
+    ]
+)
 def test_shape(data_path, design_value_name, keys, shape):
     ds = read_data(data_path, design_value_name, keys)
     assert(ds[design_value_name].shape == shape)
--- tests/test_mask.py
+++ tests/test_mask.py
@@ -1,9 +1,16 @@
-import sys, os
+import os
 import pytest
 import geopandas as gpd
 import numpy as np
+from climpyrical.mask import (
+        check_polygon_validity,
+        check_polygon_before_projection,
+        check_polygon_after_projection,
+        check_input_grid_coords,
+        rotate_shapefile,
+        gen_raster_mask_from_vector
+)
 
-from climpyrical.mask import check_polygon_validity, check_polygon_before_projection, check_polygon_after_projection, check_input_grid_coords, rotate_shapefile, gen_raster_mask_from_vector
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 
 canada = gpd.read_file(os.path.join(THIS_DIR, 'data/canada.geojson')).geometry
@@ -20,11 +27,28 @@ rotated_crs = {
         'to_meter': 0.0174532925199,
         'no_defs': True
     }
-rotated_canada = canada.to_crs(rotated_crs)
-transformed_world = gpd.read_file(os.path.join(THIS_DIR, './data/transformed_world.geojson')).geometry
 
-bad_polygon = gpd.read_file(os.path.join(THIS_DIR, './data/bad_polygon.geojson')).geometry
-good_polygon = gpd.read_file(os.path.join(THIS_DIR, './data/good_polygon.geojson')).geometry
+rotated_canada = canada.to_crs(rotated_crs)
+
+transformed_world = gpd.read_file(
+                        os.path.join(
+                            THIS_DIR,
+                            './data/transformed_world.geojson')
+                    ).geometry
+
+bad_polygon = gpd.read_file(
+                    os.path.join(
+                        THIS_DIR,
+                        './data/bad_polygon.geojson'
+                    )
+                ).geometry
+
+good_polygon = gpd.read_file(
+                    os.path.join(
+                        THIS_DIR,
+                        './data/good_polygon.geojson'
+                    )
+                ).geometry
 
 
 @pytest.mark.parametrize('p,passed', [
@@ -55,6 +79,7 @@ def test_check_polygon_before_projection(p, passed):
         with pytest.raises((TypeError, ValueError)):
             check_polygon_before_projection(p)
 
+
 @pytest.mark.parametrize('p,passed', [
     ({'string'}, False),
     ({5}, False),
@@ -70,19 +95,39 @@ def test_check_polygon_after_projection(p, passed):
             check_polygon_after_projection(p)
 
 
-@pytest.mark.parametrize('x,y,passed', [
-    ('x', np.linspace(-24, 24, 155), False),
-    (np.linspace(-24, 24, 155), 'y', False),
-    (np.linspace(0, 10, 30), np.linspace(-28.59999656677246, 28.15999984741211, 130), False),
-    (np.linspace(-33.8800048828125, 33.8800048828125, 155), np.linspace(0, 10, 130), False),
-    (np.linspace(-33.8800048828125, 33.8800048828125, 155), np.linspace(-28.59999656677246, 28.15999984741211, 130), True),
-    (np.linspace(-33.8800048828125, 33.8800048828125, 15), np.linspace(-28.59999656677246, 28.15999984741211, 10), False)])
+@pytest.mark.parametrize(
+    'x,y,passed',
+    [
+        ('x', np.linspace(-24, 24, 155), False),
+        (np.linspace(-24, 24, 155), 'y', False),
+        (
+            np.linspace(0, 10, 30),
+            np.linspace(-28.59999656677246, 28.15999984741211, 130),
+            False
+        ),
+        (
+            np.linspace(-33.8800048828125, 33.8800048828125, 155),
+            np.linspace(0, 10, 130),
+            False
+        ),
+        (
+            np.linspace(-33.8800048828125, 33.8800048828125, 155),
+            np.linspace(-28.59999656677246, 28.15999984741211, 130),
+            True
+        ),
+        (
+            np.linspace(-33.8800048828125, 33.8800048828125, 15),
+            np.linspace(-28.59999656677246, 28.15999984741211, 10),
+            False
+        )
+    ]
+)
 def test_check_input_grid_coords(x, y, passed):
     if passed:
-        assert check_input_grid_coords(x,y)
+        assert check_input_grid_coords(x, y)
     else:
         with pytest.raises((ValueError, TypeError)):
-            check_input_grid_coords(x,y)
+            check_input_grid_coords(x, y)
 
 
 @pytest.mark.parametrize('p,crs,expected', [
@@ -90,8 +135,20 @@ def test_check_input_grid_coords(x, y, passed):
 def test_rotate_shapefile(p, crs, expected):
     assert rotate_shapefile(p, crs).geom_almost_equals(expected).values[0]
 
+
 maskarray = np.load(os.path.join(THIS_DIR, './data/maskarray.npy'))
-@pytest.mark.parametrize('x,y,p,expected', [
-    (np.linspace(-33.8800048828125, 33.8800048828125, 155), np.linspace(-28.59999656677246, 28.15999984741211, 130), rotated_canada, maskarray)])
-def test_gen_raster_mask_from_vector(x,y,p,expected):
+
+
+@pytest.mark.parametrize(
+    'x,y,p,expected',
+    [
+        (
+            np.linspace(-33.8800048828125, 33.8800048828125, 155),
+            np.linspace(-28.59999656677246, 28.15999984741211, 130),
+            rotated_canada,
+            maskarray
+        )
+    ]
+)
+def test_gen_raster_mask_from_vector(x, y, p, expected):
     assert np.array_equal(gen_raster_mask_from_vector(x, y, p), expected)
