74920e09d147a55887d962a2c02dcb339b95eeee
nannau <nannau@uvic.ca>
2019-11-15 17:01:10 -0800

    flake8

--- setup.py
+++ setup.py
@@ -3,48 +3,45 @@ from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 
 
-
 class PyTest(TestCommand):
-
     def finalize_options(self):
         TestCommand.finalize_options(self)
-        self.test_args = ['-v', 'tests']
+        self.test_args = ["-v", "tests"]
         self.test_suite = True
 
     def run_tests(self):
         # import here, cause outside the eggs aren't loaded
         import pytest
+
         errno = pytest.main(self.test_args)
         sys.exit(errno)
+
 
 setup(
     name="climpyrical",
     description="A spatial basis pattern reconstruction tool",
     keywords="geography fields regression climate meteorology",
     packages=find_packages(),
-    version='0.1dev',
+    version="0.1dev",
     url="http://www.pacificclimate.org/",
     author="Nic Annau",
     author_email="nannau@uvic.ca",
     zip_safe=True,
-    scripts=[
-        'climpyrical/datacube.py',
-        'climpyrical/mask.py',
-    ],
-    install_requires=['numpy', 'shapely', 'geopandas', 'xarray'],
-    tests_require=['pytest'],
-    cmdclass={'test': PyTest},
-    package_dir={
-        'climpyircal':'climpyrical'
-    },
-    package_data={
-        'climpyrical': ['tests/data/*', 'climpyrical']
-    },
-    classifiers='''
+    scripts=["climpyrical/datacube.py", "climpyrical/mask.py"],
+    install_requires=["numpy", "shapely", "geopandas", "xarray"],
+    tests_require=["pytest"],
+    cmdclass={"test": PyTest},
+    package_dir={"climpyircal": "climpyrical"},
+    package_data={"climpyrical": ["tests/data/*", "climpyrical"]},
+    classifiers="""
 Intended Audience :: Science/Research
 License :: GNU General Public License v3 (GPLv3)
 Operating System :: OS Independent
+Programming Language :: Python :: 3.5
 Programming Language :: Python :: 3.6
+Programming Language :: Python :: 3.7
 Topic :: Scientific/Engineering
-Topic :: Software Development :: Libraries :: Python Modules'''.split('\n')
+Topic :: Software Development :: Libraries :: Python Modules""".split(
+        "\n"
+    ),
 )
