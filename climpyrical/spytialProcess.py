058cd643c790f15bd8d0474761e7d41eec406df7
Nic Annau <nicannau@gmail.com>
2020-05-29 13:08:14 -0700

    Black

--- climpyrical/spytialProcess.py
+++ climpyrical/spytialProcess.py
@@ -3,23 +3,24 @@ from collections import OrderedDict
 from nptyping import NDArray
 from typing import Any, Tuple
 import numpy as np
-from rpy2.robjects.packages import importr 
-from rpy2.robjects import FloatVector, r, DataFrame
+from rpy2.robjects.packages import importr
+from rpy2.robjects import FloatVector, DataFrame
 from rpy2 import robjects
 
-importr('fields')
+importr("fields")
 
-def fit(latlon: NDArray[(2, Any), float], 
-        z: NDArray[(Any,), float], 
-        nx: int, 
-        ny: int, 
-        xy: Tuple, 
-        distance: str, 
-        variogram_model: str) -> Tuple[
-            NDArray[(Any, Any), float], 
-            NDArray[(Any,), float], 
-            NDArray[(Any,), float]
-        ]:
+
+def fit(
+    latlon: NDArray[(2, Any), float],
+    z: NDArray[(Any,), float],
+    nx: int,
+    ny: int,
+    xy: Tuple,
+    distance: str,
+    variogram_model: str,
+) -> Tuple[
+    NDArray[(Any, Any), float], NDArray[(Any,), float], NDArray[(Any,), float]
+]:
 
     """Encapsulates the functionality of R's spatialProcess into a Python
     Args:
@@ -29,26 +30,28 @@ def fit(latlon: NDArray[(2, Any), float],
         nx: number of grid cells on interpolated grid y
         xy: dimensions of interpolated grid output
         distance: distance metric to use (note, only 'geo' supported currently)
-        variogram_model: choice of variogram model (note, only 'exoponential' supported)
+        variogram_model: choice of variogram model
+          (note, only 'exoponential' supported)
     Returns:
         z: kriged field
         x, y: locations of kriged data
 
     """
-    
+
     if latlon.shape[1] != z.size:
-        raise ValueError('Different number of grid coordinates than observations')
+        raise ValueError(
+            "Different number of grid coordinates than observations"
+        )
 
     latlon, z = latlon.tolist(), z.tolist()
 
     # convert from easier to understand names
     # to something R can understand
-    xy = 'c'+str(xy)
+    xy = "c" + str(xy)
 
     str_args_convert = {
-        'geo': "'rdist.earth'",
-        'exponential': 'list(Covariance="Exponential")',
-
+        "geo": "'rdist.earth'",
+        "exponential": 'list(Covariance="Exponential")',
     }
 
     d = str_args_convert[distance]
@@ -62,27 +65,33 @@ def fit(latlon: NDArray[(2, Any), float],
     r_lists = list(map(FloatVector, latlon))
     coords = OrderedDict(zip(map(str, range(len(r_lists))), r_lists))
     r_dataFrame = DataFrame(coords)
-    r_latlon = robjects.r['data.matrix'](r_dataFrame)
+    r_latlon = robjects.r["data.matrix"](r_dataFrame)
 
     # convert observations
-    r_z = FloatVector(z)    
+    r_z = FloatVector(z)
 
     # forced to use %-formatting due to RRuntime interpretation
-    rstring="""
-        function(latlon, z, nx, ny){
-            obj <- spatialProcess(latlon, z, Distance = %s, cov.args = %s)
-            predictSurface(obj, grid.list = NULL, extrap = FALSE, chull.mask = NA,
-                    nx = nx, ny = ny, xy = %s, verbose = FALSE, ZGrid = NULL,
-                    drop.Z = FALSE, just.fixed=FALSE)
-        }
-    """ % (d, v_model, xy)
+    rstring = """
+    function(latlon, z, nx, ny){
+        obj <- spatialProcess(latlon, z, Distance = %s, cov.args = %s)
+        predictSurface(obj, grid.list = NULL, extrap = FALSE, chull.mask = NA,
+                nx = nx, ny = ny, xy = %s, verbose = FALSE, ZGrid = NULL,
+                drop.Z = FALSE, just.fixed=FALSE)
+    }
+    """ % (
+        d,
+        v_model,
+        xy,
+    )
 
     rfunc = robjects.r(rstring)
     r_surface = rfunc(r_latlon, r_z, nx, ny)
 
     # extract data from R's interpolation
-    z = np.array(list(dict(zip(r_surface.names, list(r_surface)))['z'])).reshape(nx, ny)
-    x = np.array(list(dict(zip(r_surface.names, list(r_surface)))['x']))
-    y = np.array(list(dict(zip(r_surface.names, list(r_surface)))['y']))
+    z = np.array(
+        list(dict(zip(r_surface.names, list(r_surface)))["z"])
+    ).reshape(nx, ny)
+    x = np.array(list(dict(zip(r_surface.names, list(r_surface)))["x"]))
+    y = np.array(list(dict(zip(r_surface.names, list(r_surface)))["y"]))
 
     return z, x, y
