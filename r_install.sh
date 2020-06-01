8f3e28c310c4609e1feafb08541d44b64ae81173
Nic Annau <nicannau@gmail.com>
2020-06-01 14:31:26 -0700

    Adds .Renviron instead of config

--- r_install.sh
+++ r_install.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 # Set user library
-echo ".libPaths(c( .libPaths(), '~/.' ))" > ".Rprofile"
-echo ".libPaths()" > "config.R"
-R CMD BATCH "config.R"
+mkdir "./r-library"
+echo "~/.Renviron" > "R_LIBS_USER=./r-library"
 
 # Install packages from requirements list
 cat r_requirements.txt | while read line || [[ -n $line ]]; do
     wget "https://cran.r-project.org/src/contrib/$line.tar.gz"
-    R CMD INSTALL --library='.' "$line.tar.gz"
+    R CMD INSTALL --library='./r-library' "$line.tar.gz"
     rm "$line.tar.gz"
 done
