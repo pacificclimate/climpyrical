c33fedc4403718e13635bde6993dff803b2b63c4
Nic Annau <nicannau@gmail.com>
2020-06-01 14:11:54 -0700

    Specifies library

--- r_install.sh
+++ r_install.sh
@@ -8,6 +8,6 @@ R CMD BATCH "config.R"
 # Install packages from requirements list
 cat r_requirements.txt | while read line || [[ -n $line ]]; do
     wget "https://cran.r-project.org/src/contrib/$line.tar.gz"
-    R CMD INSTALL "$line.tar.gz"
+    R CMD INSTALL --library='.' "$line.tar.gz"
     rm "$line.tar.gz"
 done
