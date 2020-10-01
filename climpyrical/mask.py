c45967074ae583ba5bb8d5fac429ee6223049aa1
Nic Annau <nicannau@gmail.com>
2020-10-01 14:52:49 -0700

    * Relaxed type checking in favour of type hinting * Removed functions replaced by type hinting * Udpates gen_raster_mask_from_vector function * Removes strict test on polygon after transformation (only allowed one transformation) * Adds progress bar to slow functionsy

--- climpyrical/mask.py
+++ climpyrical/mask.py
@@ -1,29 +1,34 @@
+from climpyrical.gridding import (
+    find_nearest_index,
+    flatten_coords,
+)
 import warnings
-from climpyrical.gridding import check_ndims, find_nearest_index, flatten_coords
+from typing import Union, Any
+from nptyping import NDArray
 import numpy as np
-from shapely.geometry import Point, Polygon
+from shapely.geometry import Polygon
 from tqdm import tqdm
 import geopandas as gpd
 
 
-def check_polygon_validity(p):
+def check_polygon_validity(p: Union[gpd.GeoSeries, gpd.GeoDataFrame]) -> bool:
     """Checks that the polygon provided is valid
     Args:
         p: polygon of type geopandas.GeoSeries
     Raises:
-        TypeError: for wrong type
         ValueError: for GeoDataFrame that is empty or invalid values
     """
-    if not isinstance(p, gpd.GeoSeries):
-        raise TypeError("Must be gpd.GeoSeries, not {}".format(type(p)))
+
     if not p.size > 0:
-        raise ValueError("GeoDataFrame is is_empty")
+        raise ValueError("Empty data provided in polygons")
     if not p.is_valid.values[0]:
-        raise ValueError("GeoDataFrame contains invalid values.")
+        raise ValueError("Invalid values in polygons")
     return True
 
 
-def check_polygon_before_projection(p):
+def check_polygon_before_projection(
+    p: Union[gpd.GeoSeries, gpd.GeoDataFrame]
+) -> bool:
     """Raises an warning if polygon provided does not
     contain the expected WGS84 projection, but does not stop
     code from running
@@ -32,64 +37,25 @@ def check_polygon_before_projection(p):
     Returns:
         bool True if passed
     """
+    # check polygon validity
     check_polygon_validity(p)
+
     crs1 = {"init": "epsg:4326"}
     crs2 = "epsg:4326"
     if p.crs != crs1 and p.crs != crs2:
         warnings.warn(
             UserWarning(
-                f"Polygon provided is in projection {p.crs}, expected {crs1} or {crs2}"
+                f"Polygon provided is in projection {p.crs}, expected {crs1} or {crs2}.\
+                 Other transformations are experimental and have not been tested."
             )
         )
-    return True
-
-
-def check_polygon_after_projection(p):
-    """Checks that the polygon after rotation is in the correct
-    projection for rotated pole - a requirement of climpyrical
-    Args:
-        p: polygon after projection transformation
-    Returns:
-        bool True if passed
-    """
-    check_polygon_validity(p)
-
-    crs = {
-        "proj": "ob_tran",
-        "o_proj": "longlat",
-        "lon_0": -97,
-        "o_lat_p": 42.5,
-        "a": 6378137,
-        "to_meter": 0.0174532925199,
-        "no_defs": True,
-    }
-
-    if p.crs != crs:
-        raise ValueError("{} is an incorrect projection".format(p.crs))
-    return True
-
-
-def check_input_grid_coords(x, y):
-    """Checks that the input coordinates defining the CanRCM4 grid
-    are the expected type, size, and range of values.
-    Args:
-        x, y (np.ndarray): numpy arrays of rlon, rlat respectively
-            of CanRCM4 grids
-    Returns:
-        bool True if passed
-    """
-    if (not isinstance(x, np.ndarray)) or (not isinstance(y, np.ndarray)):
-        raise TypeError("Please provide an object of type {}".format(np.ndarray))
-
-    check_ndims(x, 1)
-    check_ndims(y, 1)
 
     return True
 
 
 def rotate_shapefile(
-    p,
-    crs={
+    p: Union[gpd.GeoSeries, gpd.GeoDataFrame],
+    crs: dict = {
         "proj": "ob_tran",
         "o_proj": "longlat",
         "lon_0": -97,
@@ -98,8 +64,9 @@ def rotate_shapefile(
         "to_meter": 0.0174532925199,
         "no_defs": True,
     },
-):
+) -> gpd.GeoSeries:
     """Rotates a shapefile to a new crs defined by a proj4 dictionary.
+    Uses geopandas crs functions.
     Args:
         p (geopandas.GeoSeries object): polygons of Canada
         crs (dict): proj4 dictionary
@@ -107,43 +74,53 @@ def rotate_shapefile(
         target (geopandas.GeoSeries object): geographic polygons
             in new projection
     """
-    # this checks the polyon input
-    # check_polygon_validity(p)
     # this checks polygon can be rotated
-    # check_polygon_before_projection(p)
+    check_polygon_before_projection(p)
+
     target = p.to_crs(crs)
-    # this checks the rotation
-    # check_polygon_after_projection(target)
 
     return target
 
 
-def make_box(x, y, dx, dy):
+def make_box(x: float, y: float, dx: float, dy: float) -> Polygon:
+    """Creates a Polygon box that mimics a grid cell's geometry
+    Args:
+        x, y (float, float): coordinates of center of grid cell
+        dx, dy (float, float): distance from center of grid cell to grid
+        cell's edges
+    Returns:
+        (shapely Polygon object): Polygon representation of grid
+    """
 
-    p1 = x-dx, y-dy
-    p2 = x+dx, y-dy
-    p3 = x+dx, y+dy        
-    p4 = x-dx, y+dy
-    
-    return(Polygon([p1, p2, p3, p4]))
+    p1 = x - dx, y - dy
+    p2 = x + dx, y - dy
+    p3 = x + dx, y + dy
+    p4 = x - dx, y + dy
+
+    return Polygon([p1, p2, p3, p4])
 
 
-def gen_raster_mask_from_vector(x, y, p, progress_bar=True):
+def gen_raster_mask_from_vector(
+    x: NDArray[(Any,), np.float],
+    y: NDArray[(Any,), np.float],
+    p: Union[gpd.GeoSeries, gpd.GeoDataFrame],
+    progress_bar: bool = True,
+) -> NDArray[(Any, Any), Any]:
     """Determines if points are contained within polygons of Canada
     Args:
         x, y (np.ndarray): Arrays containing the rlon and rlat of CanRCM4
             grids
         p (geopandas.GeoSeries object): rotated polygons of Canada
+        progress_bar (bool): True, whether to display a tqdm progress bar. This
+            operation can take a long time depending on the target resolution and
+            the grid size/complexity of polygons provided.
     Returns:
-        grid (np.ndarray): boolean 2D grid mask of CanRCM4 raster clipped
+        mask (np.ndarray): boolean 2D grid mask of CanRCM4 raster clipped
             based on polygon boundaries
     """
-    # this checks the coordinate inputes
-    # check_input_grid_coords(x, y)
+
     # this checks the polygon input
-    # check_polygon_validity(p)
-    # this checks that the polygon is in rotated form
-    # check_polygon_after_projection(p)
+    check_polygon_validity(p)
 
     cx1, cx2 = p.bounds.minx.min(), p.bounds.maxx.max()
     cy1, cy2 = p.bounds.miny.min(), p.bounds.maxy.max()
@@ -152,37 +129,31 @@ def gen_raster_mask_from_vector(x, y, p, progress_bar=True):
     icx1, icx2 = find_nearest_index(x, cx1), find_nearest_index(x, cx2)
     icy1, icy2 = find_nearest_index(y, cy1), find_nearest_index(y, cy2)
 
-    dx = np.mean(np.diff(x))
-    dy = np.mean(np.diff(y))
+    dx = np.mean(np.diff(x)) / 2.0
+    dy = np.mean(np.diff(y)) / 2.0
 
+    # ordered pairs of relevent grid cells
     xx, yy = flatten_coords(x[icx1:icx2], y[icy1:icy2])
     xy = np.stack([xx, yy]).T
 
+    # track whether or not grid cell is within polygon here
     contained = []
+
     if progress_bar:
         with tqdm(total=len(xy), position=0, leave=True) as pbar:
             for xcoord, ycoord in xy:
                 pbar.update()
                 contained.append(
-                    np.any(
-                        p.intersects(
-                            make_box(xcoord, ycoord, dx, dy)
-
-                        )
-                    )
+                    np.any(p.intersects(make_box(xcoord, ycoord, dx, dy)))
                 )
     else:
         for xcoord, ycoord in xy:
             contained.append(
-                np.any(
-                    p.intersects(
-                        make_box(xcoord, ycoord, dx, dy)
-
-                    )
-                )
+                np.any(p.intersects(make_box(xcoord, ycoord, dx, dy)))
             )
 
-    contained = np.array(contained).reshape((icy2-icy1, icx2-icx1))
+    # convert back to original target size and shape
+    contained = np.array(contained).reshape((icy2 - icy1, icx2 - icx1))
     mask = np.zeros((y.size, x.size))
     mask[icy1:icy2, icx1:icx2] = contained
 
