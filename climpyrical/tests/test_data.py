08b91a58a51e68fca422c2f4f867da5995983766
Nic Annau <nicannau@gmail.com>
2020-10-02 13:12:41 -0700

    Marks test shape as slow

--- climpyrical/tests/test_data.py
+++ climpyrical/tests/test_data.py
@@ -89,7 +89,7 @@ def test_valid_data(ds, error):
         with pytest.raises(error):
             check_valid_data(ds)
 
-
+@pytest.mark.slow
 @pytest.mark.parametrize(
     "data_path,design_value_name,keys,shape",
     [
