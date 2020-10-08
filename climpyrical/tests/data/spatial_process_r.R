51caa7efa464e3f2b32fc1e8d2adc4a001c3b255
Nic Annau <nicannau@gmail.com>
2020-10-08 15:49:09 -0700

    Reformats

--- climpyrical/tests/data/spatial_process_r.R
+++ climpyrical/tests/data/spatial_process_r.R
@@ -1,10 +1,28 @@
 function(latlon, z, nx, ny, extrap){
+	obj <- spatialProcess(
+		latlon, z, 
+		Distance = "rdist.earth", 
+		cov.args = list(Covariance="Exponential"),
+		verbose = FALSE
+	)
 
-	obj <- spatialProcess(latlon, z, Distance = "rdist.earth", cov.args = list(Covariance="Exponential"), verbose=FALSE, REML=FALSE)
-			
-	ps <- predictSurface(obj, grid.list = NULL, extrap = extrap, chull.mask = NA,
-	        nx = nx, ny = ny, xy = c(1, 2), verbose = FALSE, ZGrid = NULL,
-	        drop.Z = FALSE, just.fixed=FALSE)
-			rlist <- list('x' = ps$x, 'y' = ps$y, 'z' = ps$z)
+	ps <- predictSurface(
+		obj, 
+		grid.list = NULL, 
+		extrap = extrap, 
+		chull.mask = NA,
+	    nx = nx, 
+	    ny = ny, 
+	    xy = c(1, 2), 
+	    verbose = FALSE, 
+	    ZGrid = NULL,
+	    drop.Z = FALSE, 
+	    just.fixed=FALSE
+	)
+
+		
+	rlist <- list('x' = ps$x, 'y' = ps$y, 'z' = ps$z)
+	
 	return(rlist)
+
 }
