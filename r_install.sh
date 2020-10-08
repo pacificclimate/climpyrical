ceab5eab3573ddb41dbf35101ac087890d5f6c54
Nic Annau <nicannau@gmail.com>
2020-10-07 18:56:14 -0700

    Fix attempt for cancelled process

--- r_install.sh
+++ r_install.sh
@@ -7,12 +7,13 @@ echo "R_LIBS_USER=./r-library" > ".Renviron"
 # Install packages from requirements list
 cat r_requirements.txt | while read line || [[ -n $line ]]; do
 
-    wget "https://cran.r-project.org/src/contrib/$line.tar.gz"
-    if [[ "$line" == *"$Archive"* ]]; then
-	R CMD INSTALL --library='./r-library' "$(echo $line.tar.gz| cut -d'/' -f 3)"
-	rm "$(echo $line.tar.gz| cut -d'/' -f 3)"
-    else
-      R CMD INSTALL --library='./r-library' "$line.tar.gz"
-      rm "$line.tar.gz"
-    fi
+wget "https://cran.r-project.org/src/contrib/$line.tar.gz"
+if [[ "$line" == *"$Archive"* ]]; then
+R CMD INSTALL --library='./r-library' "$(echo $line.tar.gz| cut -d'/' -f 3)"
+rm "$(echo $line.tar.gz| cut -d'/' -f 3)"
+else
+R CMD INSTALL --library='./r-library' "$line.tar.gz"
+rm "$line.tar.gz"
+fi
+
 done
