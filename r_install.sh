2cb3959401aa91b67ff8dc94463594592ab23734
Nic Annau <nicannau@gmail.com>
2020-10-08 15:47:57 -0700

    Change r-library loc

--- r_install.sh
+++ r_install.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Set user library
-mkdir "./r-library"
-echo "R_LIBS_USER=./r-library" > ".Renviron"
+mkdir "./climpyrical/r-library"
+echo "R_LIBS_USER=./climpyrical/r-library" > ".Renviron"
 
 # Install packages from requirements list
 cat r_requirements.txt | while read line || [[ -n $line ]]; do
 
 wget "https://cran.r-project.org/src/contrib/$line.tar.gz"
 if [[ "$line" == *"$Archive"* ]]; then
-R CMD INSTALL --library='./r-library' "$(echo $line.tar.gz| cut -d'/' -f 3)"
+R CMD INSTALL --library='./climpyrical/r-library' "$(echo $line.tar.gz| cut -d'/' -f 3)"
 rm "$(echo $line.tar.gz| cut -d'/' -f 3)"
 else
-R CMD INSTALL --library='./r-library' "$line.tar.gz"
+R CMD INSTALL --library='./climpyrical/r-library' "$line.tar.gz"
 rm "$line.tar.gz"
 fi
 
