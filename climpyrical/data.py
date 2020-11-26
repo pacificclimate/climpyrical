07c264b39b52d45cec4afaa325ad101ee83cc0a9
Nic Annau <nicannau@gmail.com>
2020-11-26 12:03:14 -0800

    Incorporates comments

--- climpyrical/data.py
+++ climpyrical/data.py
@@ -46,82 +46,12 @@ def check_valid_data(ds: xr.Dataset) -> bool:
         if (np.any(np.diff(ds.rlat.values) < 0)) or np.any(
             (np.diff(ds.rlon.values) < 0)
         ):
-            raise ValueError(
-                "Coordinate axis are not monotonically increasing"
-            )
+            raise ValueError("Coordinate axis are not monotonically increasing")
 
     if bool(np.all(ds.to_array().isnull()).values):
         raise ValueError("All values are NaN. Check input.")
 
     return True
-
-
-def read_data(
-    data_path: str, required_keys: list = ["rlat", "rlon", "lat", "lon"]
-) -> xr.Dataset:
-    """Load NetCDF4 file. Default checks are for CanRCM4 model keys.
-
-    Note that 'rlat', 'lat', 'lon', 'rlon' are all required in addition
-    to a single data variable that contains a field of interest.
-    -----------------------------------------------------------------
-    Args:
-        data_path (Str): path to folder
-            containing CanRCM4 ensemble
-        required_keys (list, optional): list of required keys in netCDF4
-            file. Default requirements are only that it contains
-            rotated lat and rotated lon coords called rlon and rlat
-    Returns:
-        ds (xarray Dataset): dataset of netCDF4 file
-    Raises:
-        FileNotFoundError: if file not found
-        ValueError: if file contains unexpected or invalid data
-        KeyError if NetCDF4 file is missing required keys
-        TypeError if path provided is invalid
-    """
-
-    if not data_path.endswith(".nc"):
-        raise TypeError(
-            "climpyrical requires a NetCDF4 file with extension .nc"
-        )
-
-    with xr.open_dataset(data_path) as ds:
-        all_keys = list(set(ds.variables).union(set(ds.dims)))
-
-        check_valid_keys(all_keys, required_keys)
-        check_valid_data(ds)
-
-        # keys with size > 2 are good, otherwise
-        # are superfluous (time, time_bnds, rotated_pole, etc)
-        extra_keys = [key for key in all_keys if ds[key].size <= 2]
-
-        # check how many data variables with
-        # size > 2 are remaining. If more than one
-        # raise error as we can't distinguish from
-        # the intended variable.
-        dv = set(ds.data_vars) - set(extra_keys)
-        if len(dv) != 1:
-            raise KeyError(
-                "More than one data variable detected."
-                "Remove wrong one from file."
-            )
-        else:
-            (dv,) = dv
-
-        # drop an extra dimension if it snuck in
-        dvfield = ds[dv].squeeze(drop=True)
-        ds = xr.Dataset(
-            {
-                dv: (["rlat", "rlon"], dvfield),
-            },
-            coords={
-                "lat": (["rlat", "rlon"], ds.lat),
-                "lon": (["rlat", "rlon"], ds.lon),
-                "rlon": ("rlon", ds.rlon),
-                "rlat": ("rlat", ds.rlat),
-            },
-        )
-
-        return ds
 
 
 def gen_dataset(
@@ -158,6 +88,77 @@ def gen_dataset(
     )
 
     return ds
+
+
+def read_data(
+    data_path: str, required_keys: list = ["rlat", "rlon", "lat", "lon"]
+) -> xr.Dataset:
+    """Load NetCDF4 file. Default checks are for CanRCM4 model keys.
+
+    Note that 'rlat', 'lat', 'lon', 'rlon' are all required in addition
+    to a single data variable that contains a field of interest.
+    -----------------------------------------------------------------
+    Args:
+        data_path (Str): path to folder
+            containing CanRCM4 ensemble
+        required_keys (list, optional): list of required keys in netCDF4
+            file. Default requirements are only that it contains
+            rotated lat and rotated lon coords called rlon and rlat
+    Returns:
+        ds (xarray Dataset): dataset of netCDF4 file
+    Raises:
+        FileNotFoundError: if file not found
+        ValueError: if file contains unexpected or invalid data
+        KeyError if NetCDF4 file is missing required keys
+        TypeError if path provided is invalid
+    """
+
+    if not data_path.endswith(".nc"):
+        raise TypeError("climpyrical requires a NetCDF4 file with extension .nc")
+
+    with xr.open_dataset(data_path) as ds:
+        all_keys = set(ds.variables).union(set(ds.dims))
+
+        check_valid_keys(all_keys, required_keys)
+        check_valid_data(ds)
+
+        # keys with size > 2 are good, otherwise
+        # are superfluous (time, time_bnds, rotated_pole, etc)
+        extra_keys = [key for key in all_keys if ds[key].size <= 2]
+
+        # check how many data variables with
+        # size > 2 are remaining. If more than one
+        # raise error as we can't distinguish from
+        # the intended variable.
+        dv = set(ds.data_vars) - set(extra_keys)
+        if len(dv) != 1:
+            raise KeyError(
+                "Too many data variables detected."
+                f"Found {dv}, please remove the"
+                "field that is not of interest."
+            )
+        else:
+            (dv,) = dv
+
+        # drop an extra dimension if it snuck in
+        dvfield = ds[dv].squeeze(drop=True)
+
+        ds_new = gen_dataset(dv, dvfield, ds.rlat, ds.rlon, ds.lat, ds.lon)
+
+        if ds.attrs:
+            all_keys = set(ds_new.variables).union(set(ds_new.dims))
+            for key in all_keys:
+                ds_new[key].attrs = ds[key].attrs
+            attr_dict = ds.attrs
+            attr_dict["Climpyrical"] = (
+                "CanRCM4 Reconstruction contains"
+                "hybrid station and model data using"
+                "Climpyrical (https://github.com/pacificclimate/climpyrical)"
+            )
+
+            ds_new.attrs = attr_dict
+
+        return ds_new
 
 
 def interpolate_dataset(
