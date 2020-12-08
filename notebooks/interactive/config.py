c44aba4cc14eadcbc28b29f02448a00e146ffa98
Nic Annau <nicannau@gmail.com>
2020-12-08 15:03:24 -0800

    Adds updated notebooks and config

--- notebooks/interactive/config.py
+++ notebooks/interactive/config.py
@@ -1,6 +1,21 @@
-
+import matplotlib
 # comment out design values in this list 
 # you don't want to run through the pipeline
+lmap = [
+'#B544A6',
+'#884DB2',
+'#5856AF',
+'#6089AC',
+'#6AA8A2',
+'#64AE90',
+'#62B07A',
+'#75B85B',
+'#B1BF53',
+'#C78E4B',
+][::-1]
+custom_cmap = matplotlib.colors.ListedColormap(lmap)
+
+
 station_dvs = [
     "RL50 (kPa)",
     "SL50 (kPa)",
@@ -13,8 +28,8 @@ station_dvs = [
     "TwJul2.5 (degC)",
     "Tmin (degC)",
     "Tmax (degC)",
-    "Q10_Gumbel-MM",
-    "Q50_Gumbel-MM",
+    "WP10",
+    "WP50",
     "DRWP-RL5 (Pa)",
     "annual_pr (mm)",
     "annual_rain (mm)",
@@ -28,8 +43,8 @@ filenames = {
     "mean RH (%)": "RHann",
     "HDD (degC-day)": "HDD",
     "SL50 (kPa)": "SL50",
-    "Q10_Gumbel-MM": "WP10",
-    "Q50_Gumbel-MM": "WP50",
+    "WP10": "WP10",
+    "WP50": "WP50",
     "TJan2.5 (degC)": "TJan2.5",
     "TJan1.0 (degC)": "TJan1.0",
     "Tmin (degC)": "Tmin",
@@ -49,8 +64,8 @@ model_paths = {
     'mean RH (%)': 'data/model_inputs/hurs_CanRCM4-LE_ens15_1951-2016_ensmean.nc',
     'HDD (degC-day)': 'data/model_inputs/hdd_CanRCM4-LE_ens35_1951-2016_ann_ensmean.nc',
     'SL50 (kPa)': 'data/model_inputs/snw_CanRCM4-LE_ens35_1951-2016_max_rl50_load_ensmean.nc',
-    'Q10_Gumbel-MM': 'data/model_inputs/wpress_CanRCM4-LE_ens35_1951-2016_max_rl10_kpa_ensmean.nc',
-    'Q50_Gumbel-MM': 'data/model_inputs/wpress_CanRCM4-LE_ens35_1951-2016_max_rl50_kpa_ensmean.nc',
+    'WP10': 'data/model_inputs/wpress_CanRCM4-LE_ens35_1951-2016_max_rl10_kpa_ensmean.nc',
+    'WP50': 'data/model_inputs/wpress_CanRCM4-LE_ens35_1951-2016_max_rl50_kpa_ensmean.nc',
     'TJan2.5 (degC)': 'data/model_inputs/tas_CanRCM4-LE_ens35_1951-2016_1hr_jan2.5p_ensmean.nc',
     'TJan1.0 (degC)': 'data/model_inputs/tas_CanRCM4-LE_ens35_1951-2016_1hr_jan1.0p_ensmean.nc',
     'Tmax (degC)': 'data/model_inputs/tas_CanRCM4-LE_ens35_1951-2016_ann_max_ensmean.nc',
@@ -68,9 +83,9 @@ station_paths = {
     'RL50 (kPa)': 'data/station_inputs/Interim_snow_rain_load_LR_composite_stations_tbd_v4.csv',
     'mean RH (%)': 'data/station_inputs/rh_annual_mean_10yr_for_maps.csv',
     'HDD (degC-day)': 'data/station_inputs/hdd_Tmax_Tmin_allstations_v3_for_maps.csv',
-    'SL50 (kPa)': 'data/station_inputs/1day_rain_rl50_for_maps.csv',
-    'Q10_Gumbel-MM': 'data/station_inputs/wpress_stations_rl10_rl50.csv',
-    'Q50_Gumbel-MM': 'data/station_inputs/wpress_stations_rl10_rl50.csv',
+    'SL50 (kPa)': 'data/station_inputs/Interim_snow_rain_load_LR_composite_stations_tbd_v4.csv',
+    'WP10': 'data/station_inputs/wpress_stations_rl10_rl50_for_maps.csv',
+    'WP50': 'data/station_inputs/wpress_stations_rl10_rl50_for_maps.csv',
     'TJan2.5 (degC)': 'data/station_inputs/janT2.5p_T1.0p_allstations_v3_min8yr_for_maps.csv',
     'TJan1.0 (degC)': 'data/station_inputs/janT2.5p_T1.0p_allstations_v3_min8yr_for_maps.csv',
     'Tmin (degC)': 'data/station_inputs/Interim_hdd_Tmax_Tmin_delivered.csv',
@@ -80,6 +95,27 @@ station_paths = {
     'DRWP-RL5 (Pa)': 'data/station_inputs/drwp_rl5_for_maps.csv',
     'annual_pr (mm)': 'data/station_inputs/pr_annual_mean_doy_MSC_25yr_for_maps.csv',
     'annual_rain (mm)': 'data/station_inputs/rain_annual_mean_doy_MSC_25yr_for_maps.csv',
-    'RL50 (mm)':'data/station_inputs/Interim_snow_rain_load_LR_composite_stations_tbd_v4.csv'
+    'RL50 (mm)':'data/station_inputs/Interim_snow_rain_load_LR_composite_stations_tbd_v4.csv',
+    'moisture_index':'data/station_inputs/moisture_index_for_maps.csv'
 }
 
+plot_dict = {
+    'RL50 (kPa)': (custom_cmap, True, 2),
+    'mean RH (%)': (custom_cmap, False, 0),
+    'HDD (degC-day)': ('RdBu_r', False, 0),
+    'SL50 (kPa)': (custom_cmap, False, 0),
+    'WP10': (custom_cmap, False, 2),
+    'WP50': (custom_cmap, False, 2),
+    'TJan2.5 (degC)': ('RdBu_r', False, 0),
+    'TJan1.0 (degC)': ('RdBu_r', False, 0),
+    'Tmin (degC)': ('RdBu_r', False, 0),
+    'Tmax (degC)': ('RdBu_r', False, 0),
+    'TJul2.5 (degC)': ('RdBu_r', False, 0),
+    'TwJul2.5 (degC)': ('RdBu_r', False, 0),
+    'annual_rain (mm)': (custom_cmap, True, 0),
+    'annual_pr (mm)': (custom_cmap, True, 0),
+    'DRWP-RL5 (Pa)': (custom_cmap, False, 0),
+    'RL50 (mm)': (custom_cmap, True, 0),
+    'moisture_index': (custom_cmap, False, 2),
+    "Gum-LM RL10 (mm)": (custom_cmap, True, 1)
+}
